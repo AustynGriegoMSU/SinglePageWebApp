dba6d5f1b48758cc8519a36749fb5d8614b53e25
Austyn Griego <bohica2154@outlook.com>
2025-08-26 23:37:36 +0000

    idk

--- script.js
+++
@@ -1,4 +0,0 @@
-function performCalculations() {
-    var inputElement = document.getElementById('Bill');
-    var value = inputElement.value;
-    document.getElementById('tip20').innerHTML = value * 1.20;}
